9ce244c468c7d600422d1368b59960ff11c99c48
Karlee Little <karleelittle1@gmail.com>
2022-05-19 01:53:01 -0700

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello World";  
+	}
+	
+	@RequestMapping("/new_page")
+	public String route() {
+		return "Welcome to the next page";
+	}
+	
+	
+}
